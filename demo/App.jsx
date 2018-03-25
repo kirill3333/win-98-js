3cfd5b5882da23bced5ac078e9e5ab63df91613c
kirill.novikov <kirill3333@inbox.ru>
2018-03-25 19:26:32 +0300

    demo app

--- demo/App.jsx
+++ demo/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import Window from '../src/components/Window';
+
 class App extends React.Component {
   render() {
-    return <h1>Hello World</h1>;
+    return <Window width={1000} height={100} />;
   }
 }
 
